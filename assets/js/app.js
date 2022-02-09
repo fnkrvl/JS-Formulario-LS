c45d5af3fbab01e840c7d6776c76da8027c6ce9b
Franco Vicentin <franco.rvl91@gmail.com>
2022-02-09 20:02:58 -0300

    Publish a tweet

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+// Variables
+const listaTweets = document.getElementById('lista-tweets');
+
+// Event Listeners
+eventListeners();
+
+function eventListeners(){
+    // Sending the form
+    document.querySelector('#formulario').addEventListener('submit', agregarTweet);
+}
+
+
+// Funtions
+function agregarTweet(e) {
+    e.preventDefault();
+    // Read the value of the textarea
+    const tweet = document.getElementById('tweet').value;
+    // Create element and add the context to the list
+    const li = document.createElement('li');
+    li.innerHTML = tweet;
+    listaTweets.appendChild(li);
+
+    console.log(tweet);
+}
