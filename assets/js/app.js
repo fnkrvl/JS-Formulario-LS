bc9eb939177f7cd0b5b0897cdd1ac984b7bc0cdd
Franco Vicentin <franco.rvl91@gmail.com>
2022-02-09 20:23:19 -0300

    Create delete button

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,10 +15,17 @@ function agregarTweet(e) {
     e.preventDefault();
     // Read the value of the textarea
     const tweet = document.getElementById('tweet').value;
-    // Create element and add the context to the list
+    // Create delete button
+    const deleteButton = document.createElement('a');
+    deleteButton.classList = 'delete-tweet';
+    deleteButton.innerHTML = 'X'; 
+
+    // Create element and add the content to the list
     const li = document.createElement('li');
     li.innerHTML = tweet;
+    // Add the delete button to the tweet
+    li.appendChild(deleteButton);
+    // Add the tweet to the list
     listaTweets.appendChild(li);
 
-    console.log(tweet);
 }
